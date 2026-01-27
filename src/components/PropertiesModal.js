d2c820becd115a4829f1d5e00b6f328e2708e1fa
asantagata <asantagata225@gmail.com>
2026-01-27 13:58:29 -0500

    properties modal

--- src/components/PropertiesModal.js
+++ src/components/PropertiesModal.js
@@ -1,9 +1,27 @@
 import context from "../utils/context.js";
+import NumberInput from "./input/NumberInput.js";
 
 export default function FileModal() {
     return {
-        children: {
-            
-        }
+        children: [
+            {
+                class: 'option-wrapper',
+                children: [
+                    'Width',
+                    NumberInput(() => context.icon.width, (val) => {
+                        context.icon.width = val; context.commit();
+                    }, {min: 0})
+                ]
+            },
+            {
+                class: 'option-wrapper',
+                children: [
+                    'Height',
+                    NumberInput(() => context.icon.height, (val) => {
+                        context.icon.height = val; context.commit();
+                    }, {min: 0})
+                ]
+            }
+        ]
     };
 }
